a4f2b88ee8a43b10a24636a88d11c49465a9d93d
yuanli <2865437316@qq.com>
2022-12-19 15:36:52 +0800

    首页使用mycard组件

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import {Button, Input} from "antd";
 import {Link} from "@umijs/renderer-react";
 import Title from "@/components/title";
 import './index.css'
+import MyCard from "@/components/my-card";
 
 export default function Index() {
     return (
@@ -13,84 +14,13 @@ export default function Index() {
                 prefix={<SearchOutlined className="site-form-item-icon"/>}
                 size={'large'}
             />
-            <div className={'my-12 pt-9 p-6 grid gap-4 grid-cols-2 shadow-lg bg-white rounded-lg relative border-2 md:grid-cols-4'}>
-                <div className={'card-title'}>
-                    <AppstoreOutlined className={'mr-1'}/>
-                    <span className={'font-bold'}>常用工具</span>
-                </div>
+            <MyCard title={'常用工具'} icon={<AppstoreOutlined/>}>
                 <Link to="/docs" className={'inline-grid'}>
                     <Button size={'large'}>
                         密码生成器
                     </Button>
                 </Link>
-                <Link to="/docs" className={'inline-grid'}>
-                    <Button size={'large'}>
-                        密码生成器
-                    </Button>
-                </Link>
-                <Link to="/docs" className={'inline-grid'}>
-                    <Button size={'large'}>
-                        密码生成器
-                    </Button>
-                </Link>
-                <Link to="/docs" className={'inline-grid'}>
-                    <Button size={'large'}>
-                        密码生成器
-                    </Button>
-                </Link>
-                <Link to="/docs" className={'inline-grid'}>
-                    <Button size={'large'}>
-                        密码生成器
-                    </Button>
-                </Link>
-                <Link to="/docs" className={'inline-grid'}>
-                    <Button size={'large'}>
-                        密码生成器
-                    </Button>
-                </Link>
-            </div>
-            <div className={'my-12 pt-9 p-6 grid gap-4 grid-cols-4 shadow-lg bg-white rounded-lg relative border-2'}>
-                <div className={'card-title'}>
-                    <AppstoreOutlined className={'mr-1'}/>
-                    <span className={'font-bold'}>常用工具</span>
-                </div>
-                <Link to="/docs" className={'inline-grid'}>
-                    <Button size={'large'}>
-                        密码生成器
-                    </Button>
-                </Link>
-                <Link to="/docs" className={'inline-grid'}>
-                    <Button size={'large'}>
-                        密码生成器
-                    </Button>
-                </Link>
-                <Link to="/docs" className={'inline-grid'}>
-                    <Button size={'large'}>
-                        密码生成器
-                    </Button>
-                </Link>
-            </div>
-            <div className={'my-12 pt-9 p-6 grid gap-4 grid-cols-4 shadow-lg bg-white rounded-lg relative border-2'}>
-                <div className={'card-title'}>
-                    <AppstoreOutlined className={'mr-1'}/>
-                    <span className={'font-bold'}>常用工具</span>
-                </div>
-                <Link to="/docs" className={'inline-grid'}>
-                    <Button size={'large'}>
-                        密码生成器
-                    </Button>
-                </Link>
-                <Link to="/docs" className={'inline-grid'}>
-                    <Button size={'large'}>
-                        密码生成器
-                    </Button>
-                </Link>
-                <Link to="/docs" className={'inline-grid'}>
-                    <Button size={'large'}>
-                        密码生成器
-                    </Button>
-                </Link>
-            </div>
+            </MyCard>
         </div>
     );
 }

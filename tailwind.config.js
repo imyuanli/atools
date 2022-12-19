22a0d064b7d8bbdeeaf556a6e20a516f3ad244ff
yuanli <2865437316@qq.com>
2022-12-19 18:07:35 +0800

    去除tailwind 默认配置

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 module.exports = {
   content: [
+    './src/pages/*.tsx',
     './src/pages/**/*.tsx',
     './src/components/**.tsx',
     './src/components/**/*.tsx',
     './src/layouts/**.tsx',
   ],
+  corePlugins: {
+    preflight: false
+  }
 }

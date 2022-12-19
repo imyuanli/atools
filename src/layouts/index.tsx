1271535363a3e46fa2f99b03d69fd7fb9be09ef3
yuanli <2865437316@qq.com>
2022-12-19 14:22:38 +0800

    入口样式处理

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,21 +1,24 @@
-import { Link, Outlet } from 'umi';
-import styles from './index.less';
+import {Outlet} from 'umi';
+import {UserOutlined,SearchOutlined} from "@ant-design/icons";
+import {Input} from 'antd';
+import './index.css'
+import {useState} from "react";
+
+const {Search} = Input;
 
 export default function Layout() {
-  return (
-    <div className={styles.navs}>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/docs">Docs</Link>
-        </li>
-        <li>
-          <a href="https://github.com/umijs/umi">Github</a>
-        </li>
-      </ul>
-      <Outlet />
-    </div>
-  );
+    const onSearch = (value: string) => console.log(value);
+
+    // const [title,setTitle] = useState()
+    // const context={
+    //     title,
+    //     setTitle:setTitle
+    // }
+    return (
+        <div className={'main'}>
+            <div className={'content'}>
+                <Outlet/>
+            </div>
+        </div>
+    );
 }

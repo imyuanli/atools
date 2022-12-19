87dbbe311982e7bf802d4c4fd2976f00679e4b52
yuanli <2865437316@qq.com>
2022-12-19 14:22:14 +0800

    title组件

---
+++ src/components/title/index.tsx
@@ -0,0 +1,25 @@
+import {UserOutlined, HomeOutlined} from "@ant-design/icons";
+import './index.css'
+export default function Index(props: any) {
+    const {value} = props
+    return (
+        <div className={'flex justify-center items-center flex-col w-full mt-12 mb-24'}>
+            <div className={'text-2xl font-bold mb-3'}>
+                {value ? `${value} - 一个木函` : '一个木函 - 多功能效率工具箱'}
+            </div>
+            <div className={'flex justify-center items-center text-green-600'}>
+                {
+                    value &&
+                    <div className={'title-btn mr-3'}>
+                        <HomeOutlined className={'mr-1'}/>
+                        <span>返回首页</span>
+                    </div>
+                }
+                <div className={'title-btn'}>
+                    <UserOutlined className={'mr-1'}/>
+                    <span>未登录</span>
+                </div>
+            </div>
+        </div>
+    );
+}

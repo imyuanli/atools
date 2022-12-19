e9bde6e80532dae5f1ecdc1789218bdb7aa46e25
yuanli <2865437316@qq.com>
2022-12-19 17:54:59 +0800

    格式化

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,19 +1,7 @@
 import {Outlet} from 'umi';
-import {UserOutlined,SearchOutlined} from "@ant-design/icons";
-import {Input} from 'antd';
 import './index.css'
-import {useState} from "react";
-
-const {Search} = Input;
 
 export default function Layout() {
-    const onSearch = (value: string) => console.log(value);
-
-    // const [title,setTitle] = useState()
-    // const context={
-    //     title,
-    //     setTitle:setTitle
-    // }
     return (
         <div className={'main'}>
             <div className={'content'}>

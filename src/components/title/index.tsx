277e514b1a85b5b8778ebc148290069ae1efbce3
yuanli <2865437316@qq.com>
2022-12-21 17:22:51 +0800

    title组件ui修改

--- src/components/title/index.tsx
+++ src/components/title/index.tsx
@@ -1,26 +1,30 @@
-import {UserOutlined, HomeOutlined} from "@ant-design/icons";
+import {HomeOutlined, UserOutlined} from "@ant-design/icons";
 import './index.css'
 import {Link} from "@umijs/renderer-react";
 
 export default function Index(props: any) {
     const {value} = props
     return (
-        <div className={'flex justify-center items-center flex-col w-full mt-12 mb-24'}>
+        <div className={'flex-center flex-col w-full mt-12 mb-24'}>
             <div className={'text-2xl font-bold mb-3'}>
-                {value ? `${value} - 一个木函` : '一个木函 - 多功能效率工具箱'}
+                {value ? `${value} - WoodBox` : 'WoodBox - 一个多功能免费工具箱'}
             </div>
-            <div className={'flex justify-center items-center'}>
+            <div className={'flex-center'}>
                 {
                     value &&
                     <div className={'title-btn mr-3'}>
                         <HomeOutlined className={'mr-1'}/>
-                        <span><Link to="/" className={'link'}>返回首页</Link></span>
+                        <Link to="/" className={'link'}>返回首页</Link>
                     </div>
                 }
-                {/*<div className={'title-btn'}>*/}
-                {/*    <UserOutlined className={'mr-1'}/>*/}
-                {/*    <span>未登录</span>*/}
-                {/*</div>*/}
+                <div className={'title-btn mr-3'}>
+                    <HomeOutlined className={'mr-1'}/>
+                    <Link to="/" className={'link'}>返回首页</Link>
+                </div>
+                <div className={'title-btn color-text'}>
+                    <UserOutlined className={'mr-1'}/>
+                    <span>未登录</span>
+                </div>
             </div>
         </div>
     );

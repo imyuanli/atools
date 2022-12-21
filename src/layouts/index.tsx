b382b4ab11d2f941f3086b8e907fb4b3f8d4dbdf
imyuanli <2865437316@qq.com>
2022-12-21 22:52:45 +0800

    首页ui

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -4,6 +4,8 @@ import './index.css'
 export default function Layout() {
     return (
         <div className={'main'}>
+            {/*<div className={'custom-background'} />*/}
+            <div className={'cover'}></div>
             <div className={'content'}>
                 <Outlet/>
             </div>

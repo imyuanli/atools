9bc8987d73ba38b09f4573cb0bad13a61f7ffd8a
yuanli <2865437316@qq.com>
2022-12-22 14:02:42 +0800

    卡片ui

--- src/components/my-card/index.tsx
+++ src/components/my-card/index.tsx
@@ -5,7 +5,7 @@ export default function MyCard(props: any) {
     const style = 'grid gap-4 grid-cols-2 md:grid-cols-4'
     return (
         <div
-            className={`my-12 pt-9 p-6 shadow-lg bg-white rounded-lg relative border-2 ${isIndex ? style : ""}`}>
+            className={`my-12 pt-12 px-9 pb-6 shadow-lg bg-white rounded-lg relative border-2 ${isIndex ? style : ""}`}>
             {
                 title &&
                 <div className={'card-title text-base'}>

ec543f4995a94635f7cbe9c39c07fee0bf19819d
yuanli <2865437316@qq.com>
2022-12-23 16:00:33 +0800

    收藏组件

---
+++ src/components/collect/index.tsx
@@ -0,0 +1,53 @@
+import {Button, message} from "antd";
+import {StarFilled, StarOutlined} from "@ant-design/icons";
+import {useLocation} from "react-router";
+import {useEffect, useState} from "react";
+
+export default function Collect(props: any) {
+    const {routerList, setRouterList} = props
+    const location = useLocation()
+
+    //是不是首页
+    const [notIndex, setNoTIndex] = useState(true)
+
+    //是不是被收藏
+    const [isCollect, setIsCollect] = useState<any>(false)
+    useEffect(() => {
+        const {pathname} = location
+        if (pathname == "/") {
+            setNoTIndex(false)
+        } else {
+            setNoTIndex(true)
+        }
+        setIsCollect(getToolIsCollect(pathname))
+    }, [location, routerList])
+
+    //获取该工具是否被收藏
+    const getToolIsCollect = (link: any) => {
+        const res = routerList.find((item: any) => {
+            return item?.link == link
+        })
+        return res?.isCollect
+    }
+
+    //收藏和取消收藏
+    const handleChangeCollect = () => {
+        const res = routerList.find((item: any) => {
+            return item?.link == location?.pathname
+        })
+        res.isCollect = !res.isCollect
+        setRouterList([...routerList])
+        message.success(res.isCollect ? '收藏成功' : '取消收藏成功')
+    }
+    return (
+        <>
+            {
+                notIndex &&
+                <div className={'fixed right-3 bottom-3'}>
+                    <Button onClick={handleChangeCollect} shape={'circle'} size={'large'} type={'primary'} icon={
+                        isCollect ? <StarFilled/> : <StarOutlined/>}/>
+                </div>
+            }
+        </>
+    );
+}

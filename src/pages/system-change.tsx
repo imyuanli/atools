9eee0ca50d7e9a81705a4141353a3ee3fc407439
wisdom-bear <1020310566@qq.com>
2023-02-03 19:59:48 +0800

    进制转换1.0

--- src/pages/system-change.tsx
+++ src/pages/system-change.tsx
@@ -14,7 +14,6 @@ interface systemProps {
 }
 
 export default function systemChange() {
-
   const [conversionInput,setConversionInput] = useState('')
   const [systemArr,setSystemArr] = useState<systemProps[]>(
     [{
@@ -39,80 +38,55 @@ export default function systemChange() {
       number: 32
     },]
   )
+  let [result,setResult] = useState([])
   useEffect(() => {
     systemArr.map((item : any,index) => {
       const res = [...systemArr]
       if(conversionInput == '') {
         res[index].value = ''
       }else {
-        if(item.number == 2) {
-          res[index].value = parseInt(conversionInput).toString(2)
-          if(Number.isNaN(res[index].value)) res[index].value = ''
+        if (item.number == 2) {
+          res[index].value = strToBinary(conversionInput,2)
         }else if(item.number == 8) {
-          res[index].value = parseInt(conversionInput).toString(8)
+          res[index].value = strToBinary(conversionInput,8)
         }else if(item.number == 10) {
-          res[index].value = parseInt(conversionInput,10)
+          res[index].value = strToBinary(conversionInput,10)
         }else if(item.number == 16) {
-          // res[index].value = parseInt(conversionInput).toString(16)
-          res[index].value = strToHexCharCode(strToUtf8Bytes(conversionInput));
-          console.log(conversionInput,11);
-          
+          res[index].value = strToBinary(conversionInput,16)
         }else if(item.number == 32) {
-          for(let i=0;i<conversionInput.length;i++) {
-            if(res[index].value = '') {
-              res[index].value = conversionInput.charCodeAt(i).toString(32);
-            }else {
-              res[index].value += "," + conversionInput.charCodeAt(i).toString(32);
-            }
-          }
+          res[index].value = strToBinary(conversionInput,32)
         }
       }
       setSystemArr([...res])
     })
-  },[conversionInput])
+  }, [conversionInput])
 
-  function strToUtf8Bytes(str:any) {
-    const utf8:any = [];
-      for (let ii = 0; ii < str.length; ii++) {
-      let charCode = str.charCodeAt(ii);
-      if (charCode < 0x80) utf8.push(charCode);
-      else if (charCode < 0x800) {
-        utf8.push(0xc0 | (charCode >> 6), 0x80 | (charCode & 0x3f));
-      } else if (charCode < 0xd800 || charCode >= 0xe000) {
-        utf8.push(0xe0 | (charCode >> 12), 0x80 | ((charCode >> 6) & 0x3f), 0x80 | (charCode & 0x3f));
-      } else {
-        ii++;
-        charCode = 0x10000 + (((charCode & 0x3ff) << 10) | (str.charCodeAt(ii) & 0x3ff));
-        utf8.push(
-        0xf0 | (charCode >> 18),
-        0x80 | ((charCode >> 12) & 0x3f),
-        0x80 | ((charCode >> 6) & 0x3f),
-        0x80 | (charCode & 0x3f),
-        );
-      }
-      }
-      //兼容汉字，ASCII码表最大的值为127，大于127的值为特殊字符
-      for(let jj=0;jj<utf8.length;jj++){
-        var code = utf8[jj];
-        if(code>127){
-          utf8[jj] = code - 256;
-        }
-      }
-      return utf8;
+  function checkRate(input:any) { 
+    var re = /^[0-9]+.?[0-9]*/;//判断字符串是否为数字//判断正整数/[1−9]+[0−9]∗]∗/ 
+    let isNumber = re.test(input)
+    return isNumber
   }
-  function strToHexCharCode(str:any){
-		var hexCharCode = [];
-		var chars = ["0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F"];
-		for(var i = 0; i < str.length; i++) {
-			  var bit = (str[i] & 0x0f0) >> 4;
-			  hexCharCode.push(chars[bit]);
-			  var bit = str[i] & 0x0f;
-			  hexCharCode.push(chars[bit]);
-		}
-	   return hexCharCode.join("");
-  } 
-
   
+  //将字符串转换成二进制形式，中间用空格隔开
+  function strToBinary(str: any,number:number) {
+    let result = [];
+    let list = str.split(" ");
+    for(let i=0;i<list.length;i++){
+        if(i != 0){
+          result.push(" ");
+        }
+      let item = list[i];
+      var binaryStr:string = ''
+      if (checkRate(item)) {
+        binaryStr = parseInt(item).toString(number)
+      } else {
+        binaryStr = item.charCodeAt().toString(number);
+      }
+      if (binaryStr == 'NaN') continue
+      result.push(binaryStr);
+    }    
+    return result.join("");
+  }
 
   // 获取input值
   const changeConversion = (e : any) => {
@@ -124,7 +98,7 @@ export default function systemChange() {
     <div>
       <Title value={'数据转换'}/>
       <MyCard title={'进制转换'}>
-          <Input  onChange={changeConversion} value={conversionInput} style={{ width: '100%' }} allowClear defaultValue="" />
+          <Input  onChange={changeConversion} value={conversionInput} style={{ width: '100%' }} allowClear defaultValue="不同类型的值可用空格隔开" />
           <div>
             {
               systemArr.map((item,index) => {

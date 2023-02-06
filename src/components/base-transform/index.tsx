e4fa2f4fee303d328691f4d1241f17bff661cd8e
yuanli <2865437316@qq.com>
2023-02-06 17:33:28 +0800

    组件再次升级封装

--- src/components/base-transform/index.tsx
+++ src/components/base-transform/index.tsx
@@ -2,19 +2,34 @@ import * as React from "react";
 import {Input} from "antd";
 
 export default function BaseTransform(props: any) {
-    const {name, value, onChange} = props
+    const {itemArr, setCurInput} = props
+
+    //变更的方法
+    const onChange = (value: any, type: any) => setCurInput({type, value})
+
     return (
-        <div className="mt-3 p-3 rounded-lg bg-blue-50 flex-center relative">
-            <div className={'text-base'}>
-                <span>{name}：</span>
-            </div>
-            <Input
-                value={value}
-                onChange={onChange}
-                style={{width: '100%'}}
-                allowClear
-                placeholder="输入数据"
-            />
+        <div>
+            {
+                itemArr.map((item: any, index: any) => {
+                    return (
+                        <div key={index} className="mt-3 p-3 rounded-lg bg-blue-50 flex-center relative">
+                            <div className={'text-base flex-none'}>
+                                <span>{item.name}：</span>
+                            </div>
+                            <Input
+                                value={item.value}
+                                onChange={(e: any) => {
+                                    onChange(e.target.value, item.type)
+                                }}
+                                style={{width: '100%'}}
+                                allowClear
+                                placeholder={`请输入${item.name}`}
+                            />
+                        </div>
+                    )
+                })
+            }
         </div>
+
     );
 }

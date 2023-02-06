51b2c69bb83e6846db951b04aebecd552caaafe2
yuanli <2865437316@qq.com>
2023-02-06 15:42:52 +0800

    基础组件的封装

--- src/components/base-transform/index.tsx
+++ src/components/base-transform/index.tsx
@@ -1,16 +1,20 @@
-import Copy from "@/components/copy";
 import * as React from "react";
+import {Input} from "antd";
 
 export default function BaseTransform(props: any) {
-    const {name, value} = props
+    const {name, value, onChange} = props
     return (
-        <div className="mt-3 p-3 rounded-lg bg-blue-50 relative">
-            <div>
-                {name && <span>{name}：</span>}
-                {value}
+        <div className="mt-3 p-3 rounded-lg bg-blue-50 flex-center relative">
+            <div className={'text-base'}>
+                <span>{name}：</span>
             </div>
-            <Copy value={value}/>
+            <Input
+                value={value}
+                onChange={onChange}
+                style={{width: '100%'}}
+                allowClear
+                placeholder="输入数据"
+            />
         </div>
-
     );
 }

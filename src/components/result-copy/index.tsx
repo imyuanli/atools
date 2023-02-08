9833cbc8631bd24baffeccb0b81d75e2b25700ff
yuanli <2865437316@qq.com>
2023-02-08 11:59:57 +0800

    bug

--- src/components/result-copy/index.tsx
+++ src/components/result-copy/index.tsx
@@ -5,7 +5,7 @@ export default function ResultCopy(props: any) {
     return (
         <div className={'bg-blue-50 p-4 rounded-lg relative mt-2'}>
             <div className={'break-words'}>{result}</div>
-            <Copy value={result.toString()}/>
+            <Copy value={result}/>
         </div>
     );
 }

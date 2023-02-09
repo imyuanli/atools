77393494ba6056772752bc845b40de4f0ec05cc4
yuanli <2865437316@qq.com>
2023-02-09 17:20:42 +0800

    人生小格ui变化

--- src/pages/life-grid/index.tsx
+++ src/pages/life-grid/index.tsx
@@ -12,6 +12,8 @@ import {useLocation} from "react-router";
 import queryString from 'query-string';
 import {Button, DatePicker} from "antd";
 import {Link} from "@umijs/renderer-react";
+import Readme from "@/components/readme";
+import Explain from "@/components/explain";
 
 type Props = {};
 const LifeGrid = (props: Props) => {
@@ -229,6 +231,44 @@ const LifeGrid = (props: Props) => {
                 </div>
               </MyCard>
             }
+            <Readme>
+                <Explain>
+                    假设我们的寿命是 80 岁,分为400个方块。
+                </Explain>
+                <Explain>
+                    <div className={'block past'}/>
+                    <span>
+                        你已经走过的生命
+                    </span>
+                </Explain>
+                <Explain>
+                    <div className={'block sleep'}/>
+                    <span>如果你平均每天休息 8 小时，这是你余下生命里睡眠占用的时间</span>
+                </Explain>
+                <Explain>
+                    <div className={'block work'}/>
+                    <span>如果你 65 岁退休，退休前平均每天工作 8 小时，这是你余下生命里工作占用的时间</span>
+                </Explain>
+                <Explain>
+                    <div className={'block retired'}/>
+                    <span>65 岁，你退休了</span>
+                </Explain>
+                <Explain>
+                    <div className={'block child'}/>
+                    <span>如果你 28 岁生孩子, 孩子18岁出门上大学, 这 18 年里你平均每天能花 5 个小时陪伴孩子，这里是你余下生命里所用去的时间</span>
+                </Explain>
+                <Explain>
+                    <div className={'block parents'}/>
+                    <span>如果你每个月能看望父母一天，在他们 80 岁前，这是你的余生里还能陪伴他们的时光</span>
+                </Explain>
+                <Explain>
+                    <div className={'block surplus'}/>
+                    <span>除了以上之外，你剩下的所有日子</span>
+                </Explain>
+                <Explain>
+                    数据仅供娱乐，人生苦短，继续努力吧~
+                </Explain>
+            </Readme>
         </div>
     );
 };

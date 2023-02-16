fb9ccb6f75d58e944198113e927562ca4480bf67
yuanli <2865437316@qq.com>
2023-02-16 18:21:17 +0800

    用户中心

---
+++ src/pages/user/index.tsx
@@ -0,0 +1,13 @@
+import Title from "@/components/title";
+import MyCard from "@/components/my-card";
+
+export default function BingImage() {
+    return (
+        <div>
+            <Title value={'用户中心'}/>
+            <MyCard>
+                用户中心
+            </MyCard>
+        </div>
+    );
+}

2f5e727161f0c669967fa59b2c249df680b2fb11
yuanli <2865437316@qq.com>
2023-02-17 15:52:34 +0800

    开启dva

--- .umirc.ts
+++ .umirc.ts
@@ -3,9 +3,9 @@ import {defineConfig} from "umi";
 export default defineConfig({
     npmClient: "yarn",
     tailwindcss: {},
-    plugins: ["@umijs/plugins/dist/tailwindcss"],
+    plugins: ["@umijs/plugins/dist/tailwindcss", "@umijs/plugins/dist/dva"],
     theme: {
-        "@primary-color": '#b37feb',
-        "@link-color": "#b37feb"
-    }
-})
+        "@primary-color": "#b37feb",
+        "@link-color": "#b37feb",
+    },
+});

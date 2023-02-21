69203aa997873a7cc2915436332f1465b5fedf21
yuanli <2865437316@qq.com>
2023-02-21 14:33:25 +0800

    按钮判断

--- src/components/title/index.tsx
+++ src/components/title/index.tsx
@@ -2,11 +2,18 @@ import {UserOutlined, HomeOutlined, BugOutlined} from "@ant-design/icons";
 import './index.css'
 import {Link} from "@umijs/renderer-react";
 import {useTitle} from "ahooks";
+import {useEffect, useState} from "react";
+import store from 'store'
 
 export default function Title(props: any) {
     const {value, isLogin} = props
     //设置页面的title
     useTitle(value ? value : 'WoodBox')
+    const [token, setToken] = useState()
+    useEffect(() => {
+        setToken(store.get('token'))
+    }, [])
+
     return (
         <div className={'flex-center flex-col w-full my-12'}>
             <div className={'text-3xl font-bold mb-3'}>
@@ -25,10 +32,18 @@ export default function Title(props: any) {
                 {
                     !isLogin &&
                   <>
-                    <Link to={'/login'} className={'mr-3'}>
-                      <UserOutlined/>
-                      <span className={'ml-1'}>未登录</span>
-                    </Link>
+                      {
+                          token ?
+                              <Link to={'/user'} className={'mr-3'}>
+                                  <UserOutlined/>
+                                  <span className={'ml-1'}>用户中心</span>
+                              </Link>
+                              :
+                              <Link to={'/login'} className={'mr-3'}>
+                                  <UserOutlined/>
+                                  <span className={'ml-1'}>未登录</span>
+                              </Link>
+                      }
                     <Link to={'/'} className={'mr-3'}>
                       <BugOutlined/>
                       <span className={'ml-1'}>我要反馈</span>

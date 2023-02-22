59468aeae8e9ba0586762ed9eb44fca022f66c46
yuanli <2865437316@qq.com>
2023-02-22 15:36:34 +0800

    vip 及 error判断

--- src/components/title/index.tsx
+++ src/components/title/index.tsx
@@ -2,18 +2,43 @@ import {UserOutlined, HomeOutlined, BugOutlined} from "@ant-design/icons";
 import './index.css'
 import {Link} from "@umijs/renderer-react";
 import {useTitle} from "ahooks";
-import {useEffect, useState} from "react";
+import {useEffect} from "react";
 import store from 'store'
+import {useLocation, useNavigate, useSelector} from "@@/exports";
+import {message} from "antd";
 
 export default function Title(props: any) {
+    const token = store.get('token')
+    //根据状态自动判断
+    const toolArr = useSelector((state: any) => state.tools.toolArr);
+    const location = useLocation()
+    const navigate = useNavigate()
+    useEffect(() => {
+        const {pathname} = location
+        const res = toolArr.find((item: any) => pathname == `/tools/${item?.type}/${item?.link}`)
+        //报错的工具
+        if (res?.state == "error") {
+            message.error('工具正在维护，不能访问')
+            navigate('/', {replace: true})
+        }
+        //vip
+        if (res?.state == "vip") {
+            //如果没登陆
+            if (!token) {
+                message.warn('请先登陆一下')
+                navigate(`/login?redirect=${pathname}`, {replace: true})
+                return
+            }
+            //登陆后鉴权
+            else {
+                console.log("权限")
+            }
+        }
+    }, [])
+
     const {value, isLogin} = props
     //设置页面的title
     useTitle(value ? value : 'WoodBox')
-    const [token, setToken] = useState()
-    useEffect(() => {
-        setToken(store.get('token'))
-    }, [])
-
     return (
         <div className={'flex-center flex-col w-full my-12'}>
             <div className={'text-3xl font-bold mb-3'}>

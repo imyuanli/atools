d5d9a18abaa56d34f01e9cfbe6ca0ca858f224cc
yuanli <2865437316@qq.com>
2023-02-22 15:02:06 +0800

    路由地址

--- src/components/router-btn/index.tsx
+++ src/components/router-btn/index.tsx
@@ -29,7 +29,7 @@ export default function RouterBtn(props: any) {
                                     </Tooltip>
                                     :
                                     <Link
-                                        to={`/tools/${item?.type}${item?.link}`}
+                                        to={`/tools/${item?.type}/${item?.link}`}
                                         className={'relative block'}
                                     >
                                         <Button

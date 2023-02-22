795a4b95fc0b25721198ad580a6de287d8d0adaf
yuanli <2865437316@qq.com>
2023-02-22 13:17:54 +0800

    用户中心

--- src/pages/user/index.tsx
+++ src/pages/user/index.tsx
@@ -1,96 +1,69 @@
 import Title from "@/components/title";
 import MyCard from "@/components/my-card";
-import {useEffect} from "react";
 import {useSetState} from "ahooks";
-import {get_user_info} from "@/service/service";
-import Loading from "@/components/loading";
 import {Avatar, Button, Divider, Input, Tag} from "antd";
 import {FormOutlined, GiftOutlined, UserOutlined} from "@ant-design/icons";
 import dayjs from "dayjs";
+import {useSelector} from "@@/exports";
 
 export default function User() {
+    const userInfo = useSelector((state:any) => state.user.userInfo);
     const [data, setData] = useSetState({
-        loading: false,
-        userInfo: null,
         userName: "",
         isChange: false,
     })
-    const {userInfo, loading, isChange}: { userInfo: any, loading: boolean, isChange: boolean } = data
-    useEffect(() => {
-        setData({
-            loading: true
-        })
-        get_user_info().then(
-            (res: any) => {
-                if (!res.errno) {
-                    setData({
-                        userInfo: res,
-                        loading: false,
-                    })
-                } else setData({loading: false})
-            }
-        ).catch(() => {
-            setData({loading: false})
-        })
-    }, [])
-
-
+    const {isChange}: {isChange: boolean } = data
     return (
         <div>
             <Title value={'用户中心'} isLogin={true}/>
-            {
-                loading ?
-                    <Loading/>
-                    :
-                    <MyCard>
-                        <div>
-                            <div className={'flex-center flex-col mb-6'}>
-                                <div className={'mb-6'}>
-                                    <Avatar size={64} icon={<UserOutlined/>}/>
-                                </div>
-                                <div className={'flex-center w-full mb-3 text-base'}>
-                                    {
-                                        isChange?
-                                            <Input style={{width:300}}  placeholder="Basic usage" />
-                                            :
-                                            <div>
-                                                <span>{userInfo?.user_name}</span>
-                                                <FormOutlined
-                                                    onClick={() => {
-                                                        setData({
-                                                            isChange:true,
-                                                            userName:userInfo?.user_name
-                                                        })
-                                                    }}
-                                                    className={'ml-1 cursor-pointer hover:text-purple-400'}
-                                                />
-                                            </div>
-                                    }
-                                </div>
-                                <div className={'flex-center text-sm'}>
+            <MyCard>
+                <div>
+                    <div className={'flex-center flex-col mb-6'}>
+                        <div className={'mb-6'}>
+                            <Avatar size={64} icon={<UserOutlined/>}/>
+                        </div>
+                        <div className={'flex-center w-full mb-3 text-base'}>
+                            {
+                                isChange?
+                                    <Input style={{width:300}}  placeholder="Basic usage" />
+                                    :
                                     <div>
-                                        <Tag>注册时间</Tag>
-                                        <span>{dayjs(userInfo?.create_time).format('YYYY-MM-DD')}</span>
+                                        <span>{userInfo?.user_name}</span>
+                                        <FormOutlined
+                                            onClick={() => {
+                                                setData({
+                                                    isChange:true,
+                                                    userName:userInfo?.user_name
+                                                })
+                                            }}
+                                            className={'ml-1 cursor-pointer hover:text-purple-400'}
+                                        />
                                     </div>
-                                    <Divider type={'vertical'}/>
-                                    <div>
-                                        <Tag>FTID</Tag>
-                                        <span>{userInfo?.ft_id}</span>
-                                    </div>
-                                    <Divider type={'vertical'}/>
-                                    <div>
-                                        <GiftOutlined className={'text-xl mr-1'}/>
-                                        <span>未开通会员</span>
-                                    </div>
-                                </div>
+                            }
+                        </div>
+                        <div className={'flex-center text-sm'}>
+                            <div>
+                                <Tag>注册时间</Tag>
+                                <span>{dayjs(userInfo?.create_time).format('YYYY-MM-DD')}</span>
                             </div>
-                            <div className={'flex justify-between w-full'}>
-                                <div></div>
-                                <Button danger>退出账号</Button>
+                            <Divider type={'vertical'}/>
+                            <div>
+                                <Tag>FTID</Tag>
+                                <span>{userInfo?.ft_id}</span>
+                            </div>
+                            <Divider type={'vertical'}/>
+                            <div>
+                                <GiftOutlined className={'text-xl mr-1'}/>
+                                <span>未开通会员</span>
                             </div>
                         </div>
-                    </MyCard>
-            }
+                    </div>
+                    <div className={'flex justify-between w-full'}>
+                        <div></div>
+                        <Button danger>退出账号</Button>
+                    </div>
+                </div>
+            </MyCard>
         </div>
     );
 }

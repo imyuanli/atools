d405d27bd666eb21e915e5aa8e1620ff3e6ae1e2
yuanli <2865437316@qq.com>
2023-02-22 13:16:30 +0800

    dva 逻辑

--- src/models/user.ts
+++ src/models/user.ts
@@ -6,9 +6,6 @@ export default {
         userInfo: null,
     },
     reducers: {
-        getUserInfo(state: any) {
-            return state
-        },
         setUserInfo(state: any, {payload}: any) {
             state.userInfo = payload
             return state
@@ -17,14 +14,20 @@ export default {
     effects: {
         * getUserInfo({payload}: any, {put, call}: any) {
             // @ts-ignore
-            const res: any = yield call(get_user_info);
+            const res:any = yield call(get_user_info);
             yield put({type: 'setUserInfo', payload: res})
         },
     },
-    subscriptions: {    // 该选项中的函数自定义命名?函数的触发时机是初始时，主要用来初始化模块状态或者做一些准备性工作
-        // 注意：subscription 需要返回 unlisten 方法，用于取消数据订阅。
-        setup({history, dispatch}: any) {
+    subscriptions: {
+        setup({ dispatch}:any) {
             dispatch({type: 'getUserInfo'});
         },
-    },
+        setupHistory({dispatch,history}:any){
+            history.listen(({location}:any) => {
+                if(location.pathname == '/user'){
+                    dispatch({type: 'getUserInfo'});
+                }
+            })
+        }
+    }
 };

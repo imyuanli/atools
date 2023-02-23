fa589822a481717ebc50a9a6fe8822c263b8b1d4
yuanli <2865437316@qq.com>
2023-02-23 17:51:19 +0800

    控制台权限验证

--- src/pages/console/index.tsx
+++ src/pages/console/index.tsx
@@ -1,9 +1,12 @@
 import {useNavigate} from "@@/exports";
 import {useEffect} from "react";
+import withAuthRole from "@/hocs/withAuthRole";
 
-export default function Console() {
+function Console() {
     const navigate = useNavigate()
     useEffect(() => {
         navigate('/console/tool')
     }, [])
 }
+
+export default withAuthRole(Console)

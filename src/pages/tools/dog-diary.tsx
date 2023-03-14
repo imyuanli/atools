c1a9444e185618b5d72560578cca52a68dfb6bee
yuanli <2865437316@qq.com>
2023-03-14 13:32:05 +0800

    用组件代替

--- src/pages/tools/dog-diary.tsx
+++ src/pages/tools/dog-diary.tsx
@@ -1,52 +1,6 @@
-import Title from "@/components/title";
-import MyCard from "@/components/my-card";
-import {useEffect, useState} from "react";
 import {get_dog_diary} from "@/service/service";
-import Loading from "@/components/loading";
-import Readme from "@/components/readme";
-import Explain from "@/components/explain";
-import ApiBtn from "@/components/api-btn";
-import Copy from "@/components/copy";
+import ASentence from "@/components/a sentence";
 
 export default function DogDiary() {
-    const [dogMessage, setDogMessage] = useState("")
-    const getDogMessage = () => {
-        setDogMessage("")
-        get_dog_diary().then((res: any) => {
-            if (!res.errno) setDogMessage(res)
-            else setDogMessage("")
-        }).catch((e) => {
-            setDogMessage("")
-        })
-    }
-    useEffect(() => {
-        getDogMessage()
-    }, [])
-    return (
-        <div>
-            <Title value={'舔狗日记'}/>
-            <MyCard>
-                {
-                    dogMessage ?
-                        <div className={'relative'}>
-                            <Copy value={dogMessage}/>
-                            <div className={'p-16 text-xl whitespace-pre-wrap'}>
-                                {dogMessage}
-                            </div>
-                            <ApiBtn
-                                text={'再来亿篇'}
-                                func={getDogMessage}
-                            />
-                        </div>
-                        :
-                        <Loading/>
-                }
-            </MyCard>
-            <Readme>
-                <Explain>
-                    舔一个是狗！舔一群是狼！加油成为战狼吧！
-                </Explain>
-            </Readme>
-        </div>
-    );
+    return (<ASentence title={'舔狗日记'} api={get_dog_diary} text={'再来亿篇'} readme={'舔一个是狗！舔一群是狼！加油成为战狼吧！'}/>);
 }
--- src/pages/tools/toxic-soul.tsx
+++ src/pages/tools/toxic-soul.tsx
@@ -1,52 +1,6 @@
-import Title from "@/components/title";
-import MyCard from "@/components/my-card";
-import {useEffect, useState} from "react";
-import Loading from "@/components/loading";
-import Readme from "@/components/readme";
-import Explain from "@/components/explain";
-import ApiBtn from "@/components/api-btn";
-import Copy from "@/components/copy";
 import {get_soul_message} from "@/service/service";
+import ASentence from "@/components/a sentence";
 
 export default function ToxicSoul() {
-    const [message, setMessage] = useState("")
-    const getSoulMessage = () => {
-        setMessage("")
-        get_soul_message().then((res: any) => {
-            if (!res.errno) setMessage(res)
-            else setMessage("")
-        }).catch((e) => {
-            setMessage("")
-        })
-    }
-    useEffect(() => {
-        getSoulMessage()
-    }, [])
-    return (
-        <div>
-            <Title value={'心灵毒鸡汤'}/>
-            <MyCard>
-                {
-                    message ?
-                        <div className={'relative'}>
-                            <Copy value={message}/>
-                            <div className={'p-16 text-xl whitespace-pre-wrap flex justify-center  items-center'}
-                                 dangerouslySetInnerHTML={{__html: message}}
-                            />
-                            <ApiBtn
-                                text={'再来亿碗'}
-                                func={getSoulMessage}
-                            />
-                        </div>
-                        :
-                        <Loading/>
-                }
-            </MyCard>
-            <Readme>
-                <Explain>
-                    鸡汤虽好可不要贪杯哦~
-                </Explain>
-            </Readme>
-        </div>
-    );
+    return (<ASentence title={'心灵毒鸡汤'} api={get_soul_message} text={'再来亿碗'} readme={'鸡汤虽好可不要贪杯哦~'}/>);
 }
